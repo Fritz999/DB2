a63f5a13fe6a668f3374ddc08dc9c33b7b8a7db6
abrunner <brunner@student.tugraz.at>
2019-01-06 17:17:43 +0100

    vorletzte hypothese sql

---
+++ MariaDB/hypothese_alex.sql
@@ -0,0 +1,15 @@
+SELECT
+(
+  (
+    SELECT TIMESTAMPDIFF(SQL_TSI_SECOND, (
+  SELECT MIN(COALESCE(time_stamp)) FROM measured_values WHERE point_uid = 21
+),(
+SELECT MAX(COALESCE(time_stamp)) FROM measured_values WHERE point_uid = 21
+)) / 5
+  ) -
+  (
+  SELECT COUNT(*)
+  FROM measured_values
+  WHERE point_uid = 21
+  )
+)
